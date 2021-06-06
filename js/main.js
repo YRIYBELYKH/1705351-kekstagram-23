64e1340da50d0b618695fd73f0b60e22f6178806
yriy <y.s.belykh@yandex.ru>
2021-06-06 18:30:26 +0300

    Добавляет функции для генерации объектов

--- js/main.js
+++ js/main.js
@@ -10,10 +10,52 @@ const getRandom = (min, max) => {
   return Math.floor(Math.random() * (floorMax - ceilMin + 1)) + ceilMin;
 };
 
-getRandom(0, -1);
-
-
 // Функция для проверки максимальной длины строки
 const getMaxString = (str, maxLength) => str.length <= maxLength;
 
 getMaxString('false', 20);
+
+const NAMES = [
+  'Peter',
+  'Mary',
+  'Garry',
+  'Boris',
+  'Mia',
+  'Noah',
+  'Jacob',
+  'William',
+  'Michael',
+  'Alexander',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'В целом всё неплохо. Но не всё.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const PICTURES_COUNT = 25;
+
+const generateComments = (commentsCount) => new Array(commentsCount).fill(null).map(() => (
+  {
+    id: getRandom(10000, 20000),
+    avatar: 'img/avatar-'.concat(getRandom(1, 6)).concat('.svg'),
+    message: MESSAGES[getRandom(0, 1)],
+    name: NAMES[getRandom(0, 9)],
+  }
+));
+
+const generatePictures = (picturesCount) => new Array(picturesCount).fill(null).map((item, index) => (
+  {
+    id: index + 1,
+    url: 'photos/'.concat(index + 1).concat('.jpg'),
+    description: 'description - '.concat(index + 1),
+    likes: getRandom(15, 200),
+    comments: generateComments(getRandom(1, 10)),
+  }
+));
+
+const pictures = generatePictures(PICTURES_COUNT);
